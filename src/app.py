9ebe0f9c63aeb225eb5514d37448db5f9af364b4
gabryallaseconda <gabriele.gabrielli.96@gmail.com>
2023-09-27 17:29:08 +0200

    some corrections

--- src/app.py
+++ src/app.py
@@ -7,13 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Simple echo application</title>
+        </head>
+        <body>
+            <h1> Hello there! write here your name </h1>
+            <form action="/echo_user_input" method="POST">
+                <input name="user_input">
+                <input type="submit" value="Submit!">
+            </form>
+        </body>
+    </html>
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return "Hello " + input_text + ", pleased to meet you!"
